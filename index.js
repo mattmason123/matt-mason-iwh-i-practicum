b23683129b2bf13d376fce5b5af2dcf0319939bd
mattmason123 <mattmasonwork@gmail.com>
2025-05-20 15:19:25 +0800

    Final practicum changes

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -8,19 +11,66 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+require('dotenv').config();
+const PRIVATE_APP_ACCESS = process.env.HUBSPOT_ACCESS_TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+  const url = 'https://api.hubapi.com/crm/v3/objects/2-142901100?properties=name,description,type';
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json'
+  };
+
+  try {
+    const response = await axios.get(url, { headers });
+    const records = response.data.results;
+    res.render('homepage', { title: 'Custom Object Table | IWH Practicum', records });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error fetching custom object data');
+  }
+});
+
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+app.get('/update-cobj', (req, res) => {
+  res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
+});
+
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+  const { name, description, type } = req.body;
+
+  const url = 'https://api.hubapi.com/crm/v3/objects/2-142901100';
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json'
+  };
+
+  const newRecord = {
+    properties: {
+      name,
+      description,
+      type
+    }
+  };
+
+  try {
+    await axios.post(url, newRecord, { headers });
+    res.redirect('/');
+  } catch (error) {
+    console.error('Error creating record:', error.response?.data || error.message);
+    res.status(500).send('Error creating custom object');
+  }
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
